77d96e49e3f89211605ea5d2da68bbde3f441c05
Admin <samih1079@gmail.com>
2023-09-07 13:58:12 +0300

    design main xml todo: 1.add image 2. fixx errors line

--- app/src/main/java/com/example/tasklist2023/MainActivity.java
+++ app/src/main/java/com/example/tasklist2023/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //35323132123123
         setContentView(R.layout.activity_main);
     }
 }
