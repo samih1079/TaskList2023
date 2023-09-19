48787b1367857278288f62369c8b3fddbcda6bf1
Admin <samih1079@gmail.com>
2023-09-19 12:24:40 +0300

    عسثق يلا

--- app/src/main/java/com/example/tasklist2023/MainActivity.java
+++ app/src/main/java/com/example/tasklist2023/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tasklist2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //35323132123123
         setContentView(R.layout.activity_main);
+        Log.d("SM","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("SM","onRestart");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("SM","onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("SM","onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("SM","onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("SM","onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/example/tasklist2023/data/usersTable/MyUser.java
@@ -0,0 +1,64 @@
+package com.example.tasklist2023.data.usersTable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+//Entity = Table =جدول
+//عندما نريد ان نتعامل مع هذه الفئة كجدول معطيات
+/**
+ * فئة تمثل المستعمل
+ */
+@Entity
+public class MyUser
+{
+    @PrimaryKey(autoGenerate = true)//تحديد الصفة كمفتاح رئيسي والذي يُنتجح بشكل تلقائي
+    public long keyid;
+    @ColumnInfo(name = "full_Name")//اعطاء اسم جديد للعامود-الصفة في الجدول
+    public String fullName;
+    public String email;//بحالة لم يتم اعطاء اسم للعامود يكون اسم الصفه هو اسم العامود
+    public String phone;
+    public String passw;
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "keyid=" + keyid +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", passw='" + passw + '\'' +
+                '}';
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassw() {
+        return passw;
+    }
+
+    public void setPassw(String passw) {
+        this.passw = passw;
+    }
+}
